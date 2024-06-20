774840b0853195f145398f6af51823158c7beb31
amandaejohnston <amandaejohnston3@gmail.com>
2024-06-20 15:09:24 -0500

    add edit/delete buttons to counter page

--- src/CounterPage.tsx
+++ src/CounterPage.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { create, trash } from 'ionicons/icons';
 import { useLiveQuery } from "dexie-react-hooks";
 import { Form, useParams } from "react-router-dom";
 import { db } from "./db";
@@ -6,13 +7,29 @@ import { db } from "./db";
 function CounterPage() {
   const { id } = useParams();
   const counter = useLiveQuery(() => db.counters.get(parseInt(id!)));
-  // TODO: handle unknown counter and non-numeric param
+  // TODO: handle unknown counter and non-numeric param https://reactrouter.com/en/main/fetch/redirect
 
   return (
     <IonPage id="counter-page">
       <IonHeader>
         <IonToolbar>
           <IonTitle>{counter?.name}</IonTitle>
+          <IonButtons slot="secondary">
+            <Form action="edit">
+              <IonButton type="submit">
+                <IonIcon slot="icon-only" icon={create}></IonIcon>
+              </IonButton>
+            </Form>
+            <Form method="post" action="destroy" onSubmit={(ev) => {
+              if (!confirm('Are you sure you want to delete this counter?')) {
+                ev.preventDefault();
+              }
+            }}>
+              <IonButton type="submit">
+                <IonIcon slot="icon-only" icon={trash}></IonIcon>
+              </IonButton>
+            </Form>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
