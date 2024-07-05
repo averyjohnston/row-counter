ae43191593503ddb2a27e15c251f8fc6c491275a
amandaejohnston <amandaejohnston3@gmail.com>
2024-07-05 15:32:34 -0500

    change color input in counter form to 3rd party since Android Chrome's color picker sucks

--- src/components/CounterForm.tsx
+++ src/components/CounterForm.tsx
@@ -1,21 +1,34 @@
-import { IonList, IonItem, IonInput, IonButton, IonLabel } from "@ionic/react";
+import { IonList, IonItem, IonInput, IonButton, IonLabel, IonPopover } from "@ionic/react";
 import { Form } from "react-router-dom";
 import { CounterFormProps } from "../types";
+import { useState } from "react";
+import { HexColorInput, HexColorPicker } from "react-colorful";
+
+import './CounterForm.scss';
 
 // TODO: add field for max (should reset to resetValue once counter exceeds max)
+// TODO: new color picker can take octal codes; adjust other stuff like getContrastColor to allow for those
 
 function CounterForm(props: CounterFormProps) {
-  const { name, color, resetValue } = props;
+  const { name, color: defaultColor, resetValue } = props;
+  const [color, setColor] = useState(defaultColor);
 
   return (
-    <Form method="post">
+    <Form method="post" className="counter-form">
       <IonList>
         <IonItem>
           <IonInput label="Name" labelPlacement="floating" name="name" value={name} required></IonInput>
         </IonItem>
         <IonItem>
           <IonLabel>Color</IonLabel>
-          <input slot="end" type="color" name="color" defaultValue={color} />
+          <IonButton id="color-picker-button" className="color-button" size="small" style={{
+            '--background': color
+          }}></IonButton>
+          <IonPopover trigger="color-picker-button" className="counter-form__color-popover">
+            <HexColorPicker color={color} onChange={setColor} />
+            <HexColorInput prefixed={true} color={color} onChange={setColor} />
+          </IonPopover>
+          <input type="hidden" name="color" value={color} />
         </IonItem>
         <IonItem>
           <IonInput label="Reset Value" labelPlacement="floating" type="number" name="resetValue" value={resetValue} required></IonInput>
