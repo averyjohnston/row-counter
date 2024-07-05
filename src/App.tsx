3f265f9eefe76c7be978885085439f01d050ebb2
amandaejohnston <amandaejohnston3@gmail.com>
2024-07-05 00:14:09 -0500

    move routing logic and IonApp rendering to App component so it's not just a pathway to CounterListPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
-import { setupIonicReact } from '@ionic/react';
+import { IonApp, setupIonicReact } from '@ionic/react';
 import CounterListPage from './pages/CounterListPage';
+import { LoaderFunction, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { db } from './db';
+import ErrorPage from './pages/ErrorPage.tsx';
+import CounterPage, { action as counterPageAction } from './pages/CounterPage.tsx';
+import NewCounterPage, { action as newCounterPageAction } from './pages/NewCounterPage.tsx';
+import EditCounterPage, { action as editCounterPageAction } from './pages/EditCounterPage.tsx';
+import SettingsPage from './pages/SettingsPage.tsx';
 
 setupIonicReact();
-
-function App() {
-  return <CounterListPage />
-}
-
-export default App;
 
 // TODO: add stricter linting, especially for quote types and import order
 // TODO: dark mode switch
 // TODO: prevent device from going to sleep on any screen? (gate behind global setting)
+
+const counterLoader: LoaderFunction = async ({ params }) => {
+  const { id } = params;
+  const idNum = parseInt(id || '');
+  if (isNaN(idNum)) {
+    throw new Error(`Invalid counter ID: ${id}`);
+  }
+
+  const counter = await db.counters.get(idNum);
+  if (counter === undefined) {
+    throw new Error(`No counter matching ID: ${id}`);
+  }
+
+  return counter;
+};
+
+const router = createBrowserRouter([
+  {
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <CounterListPage />
+      },
+      {
+        path: "counters/:id",
+        element: <CounterPage />,
+        loader: counterLoader,
+        action: counterPageAction
+      },
+      {
+        path: "counters/new",
+        element: <NewCounterPage />,
+        action: newCounterPageAction
+      },
+      {
+        path: "counters/:id/edit",
+        element: <EditCounterPage />,
+        loader: counterLoader,
+        action: editCounterPageAction
+      },
+      {
+        path: "settings",
+        element: <SettingsPage />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <IonApp>
+      <RouterProvider router={router} />
+    </IonApp>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, LoaderFunction, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import CounterPage, { action as counterPageAction } from './pages/CounterPage.tsx';
-import NewCounterPage, { action as newCounterPageAction } from './pages/NewCounterPage.tsx';
-import EditCounterPage, { action as editCounterPageAction } from './pages/EditCounterPage.tsx';
-import SettingsPage from './pages/SettingsPage.tsx';
-import { IonApp } from '@ionic/react';
-import { db } from './db.ts';
 
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
@@ -23,58 +15,8 @@ import '@ionic/react/css/display.css';
 
 import './index.css';
 
-const counterLoader: LoaderFunction = async ({ params }) => {
-  const { id } = params;
-  const idNum = parseInt(id || '');
-  if (isNaN(idNum)) {
-    throw new Error(`Invalid counter ID: ${id}`);
-  }
-
-  const counter = await db.counters.get(idNum);
-  if (counter === undefined) {
-    throw new Error(`No counter matching ID: ${id}`);
-  }
-
-  return counter;
-};
-
-const router = createBrowserRouter([
-  {
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <App />
-      },
-      {
-        path: "counters/:id",
-        element: <CounterPage />,
-        loader: counterLoader,
-        action: counterPageAction
-      },
-      {
-        path: "counters/new",
-        element: <NewCounterPage />,
-        action: newCounterPageAction
-      },
-      {
-        path: "counters/:id/edit",
-        element: <EditCounterPage />,
-        loader: counterLoader,
-        action: editCounterPageAction
-      },
-      {
-        path: "settings",
-        element: <SettingsPage />
-      }
-    ]
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <IonApp>
-      <RouterProvider router={router} />
-    </IonApp>
+    <App />
   </React.StrictMode>,
 );
