4568cc2910511718ef3f8548837e911c233e7346
amandaejohnston <amandaejohnston3@gmail.com>
2024-07-09 19:17:45 -0500

    add subCounters DB

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,12 @@
 import Dexie, { type EntityTable } from 'dexie';
-import type { Counter } from './types';
+import type { Counter, SubCounter } from './types';
 
 export const db = new Dexie('CountersDatabase') as Dexie & {
-  counters: EntityTable<Counter, 'id'>
+  counters: EntityTable<Counter, 'id'>,
+  subCounters: EntityTable<SubCounter, 'id'>
 };
 
-db.version(3).stores({
-  counters: '++id, name'
+db.version(4).stores({
+  counters: '++id, name',
+  subCounters: '++id, name'
 });
