0ebe0fb0c5eda3d5cadce7371b1db28a7ef7a422
averyjohnston <averyejohnston@gmail.com>
2024-07-22 15:04:06 -0500

    add stylistic eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,11 +13,18 @@ module.exports = {
   parserOptions: {
     project: ['tsconfig.json']
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@stylistic'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@stylistic/quotes': ['warn', 'single'],
+    '@stylistic/no-extra-parens': 'warn',
+    '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+    '@stylistic/jsx-quotes': 'warn',
+    '@stylistic/spaced-comment': 'warn',
+    '@stylistic/jsx-self-closing-comp': 'warn',
+    '@stylistic/jsx-wrap-multilines': 'warn',
   },
 }
