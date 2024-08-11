9070e5e269d3c82a4109fb2ee6087ae202619e67
averyjohnston <averyejohnston@gmail.com>
2024-08-10 21:57:46 -0500

    add ButtonAction helper component

---
+++ src/components/ButtonAction.tsx
@@ -0,0 +1,43 @@
+import { IonButton } from '@ionic/react';
+import { type PropsWithChildren, useContext } from 'react';
+import { useFetcher } from 'react-router-dom';
+import type { SubmitTarget } from 'react-router-dom/dist/dom';
+
+import { globalSettingsContext } from '../App';
+import { clickVibrate } from '../utils';
+
+/**
+ * Renders a button that, when clicked, performs an imperative form
+ * submit using the specified form data. Note that for now, the submits
+ * are done in place (i.e. without navigation), though this functionality
+ * could be added in the future if needed.
+ *
+ * A Form component is not used to avoid needing to render extra
+ * unnecessary hidden inputs.
+ *
+ * If the ionButton prop is true, an IonButton component will be rendered.
+ * Otherwise, a normal button element will be used instead.
+ *
+ * If the haptics prop is true, clicking the button will cause the device
+ * to vibrate, if the global haptics setting is enabled.
+ */
+export default function ButtonAction(props: PropsWithChildren<{
+  formData?: SubmitTarget,
+  ionButton?: boolean,
+  haptics?: boolean,
+  className?: string
+}>) {
+  const { formData, ionButton, haptics, className, children } = props;
+  const { globalSettings } = useContext(globalSettingsContext);
+  const fetcher = useFetcher();
+
+  const submit = () => {
+    if (haptics && globalSettings.haptics) clickVibrate();
+    fetcher.submit(formData || null, { method: 'post' });
+  };
+
+  return ionButton ?
+    <IonButton className={className} fill="clear" onClick={submit}>{children}</IonButton> :
+    <button className={className} onClick={submit}>{children}</button>
+  ;
+}
